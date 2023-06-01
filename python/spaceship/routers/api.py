75b184f2ad55d76e126ad3e22dbd93fa88fd22f1
Dimasenchylo <84179976+Dimasenchylo@users.noreply.github.com>
2023-06-01 22:40:47 +0300

    Add numpy

--- python/spaceship/routers/api.py
+++ python/spaceship/routers/api.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+import numpy as np
 
 router = APIRouter()
 
@@ -6,3 +7,11 @@ router = APIRouter()
 @router.get('')
 def hello_world() -> dict:
     return {'msg': 'Hello, World!'}
+
+@router.get('/matrix_mult')
+def matrix() -> dict:
+    matrix_a = np.random.rand(10, 10).tolist()
+    matrix_b = np.random.rand(10, 10).tolist()
+    sum = np.dot(matrix_a, matrix_b).tolist()
+    result = {"matrix_a": matrix_a, "matrix_b": matrix_b, "total": sum}
+    return result
